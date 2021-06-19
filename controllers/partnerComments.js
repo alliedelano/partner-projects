ca86a721af594c18276502c8c2ce33d27a4175b9
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2021-06-19 09:13:59 -0700

    adds delete comment functionality for partner comments

--- controllers/partnerComments.js
+++ controllers/partnerComments.js
@@ -2,8 +2,32 @@ const Partner = require('../models/partner');
 
 module.exports = {
     create,
-    //delete: deleteComment
+    //edit,
+    //update,
+    delete: deleteComment
 }
+
+// function edit(req, res) {
+//     Partner.findOne({'comments._id': req.params.id}, function(err, partner){
+//         if(err){
+//             res.send(err);
+//         } else {
+//             res.render(`/partners/${partner._id}/comments/${req.params.id}/edit`)
+//         }
+//     })
+// }
+
+function deleteComment(req, res){
+    Partner.findOne({'comments._id': req.params.id}, function(err, partner){
+        const commentSubdoc = partner.comments.id(req.params.id);
+        //if(!commentSubdoc.enteredBy.equals(req.user._id)) return res.redirect(`/partners/${partner._id}`);
+        commentSubdoc.remove();
+        partner.save(function(err) {
+            res.redirect(`/partners/${partner._id}`);
+            })
+        })
+    }
+
 
 function create(req, res) {
     Partner.findById(req.params.id, function(err, partner){
