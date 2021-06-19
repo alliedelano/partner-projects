2b8101e74f36e73b555db81ad9965299384ac1d3
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2021-06-18 17:01:22 -0700

    adds log of team member to comment for partner, begins delete/edit for individual comments

--- controllers/partnerComments.js
+++ controllers/partnerComments.js
@@ -1,14 +1,24 @@
 const Partner = require('../models/partner');
 
 module.exports = {
-    create
+    create,
+    //delete: deleteComment
 }
 
 function create(req, res) {
     Partner.findById(req.params.id, function(err, partner){
+        req.body.enteredBy = req.user
         partner.comments.push(req.body);
         partner.save(function(err){
             res.redirect(`/partners/${partner._id}`);
         })
     })
 }
+
+// function deleteComment(req, res){
+//     Partner.findById(req.params.id, function(err, partner){
+//         partner.comments.find(function(comment){
+//             if (comment.id === req.params.comment_id)
+//         })
+//     })
+// }
