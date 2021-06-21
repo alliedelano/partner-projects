fed885a2e0a008e7df9140345ad78b4093ed30ec
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2021-06-21 15:19:22 -0700

    adds ability tto render a page to edit a comment that a user added

--- controllers/partnerComments.js
+++ controllers/partnerComments.js
@@ -9,15 +9,26 @@ module.exports = {
 }
 
 function update(req, res) {
-    console.log("i updated")
+    Partner.findOne({'comments._id': req.params.id}, function(err, partner){
+        const commentSubdoc = partner.comments.id(req.params.id);
+        if (!commentSubdoc.userId.equals(req.user._id)) return res.redirect(`/partners/${partner._id}`);
+        commentSubdoc.text = req.body.text;
+        partner.save(function(err) {
+            res.redirect(`/partners/${partner._id}`)
+        })
+    })
 }
 
 function edit(req, res) {
     Partner.findOne({'comments._id': req.params.id}, function(err, partner){
+        const commentSubdoc = partner.comments.id(req.params.id);
         if(err){
             res.send(err);
         } else {
-            res.render(`/partners/${partner._id}/comments/${req.params.id}/edit`)
+            res.render(`partners/comments/edit`, {
+                partner,
+                commentSubdoc
+            })
         }
     })
 }
