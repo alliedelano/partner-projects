1b1307a9e5937552485595aba1a41d816c05a2bd
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2021-06-22 15:48:51 -0700

    adds styling and placeholder image for homepage

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const dashboardCtrl = require('../controllers/dashboard');
 const passport = require('passport');
 
-router.get('/', dashboardCtrl.index);
+router.get('/', isLoggedIn, dashboardCtrl.index);
+
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+}
 
 module.exports = router;
