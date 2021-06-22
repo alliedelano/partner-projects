b2c5b9b05f81f4fcfdad22ac9251f9569fb6c3d5
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2021-06-22 11:36:57 -0700

    completes edit, update, delete functionality for project comments

--- controllers/projectComments.js
+++ controllers/projectComments.js
@@ -3,7 +3,30 @@ const Project = require('../models/project');
 
 module.exports = {
     create,
-    edit
+    edit,
+    update,
+    delete: deleteComment
+}
+
+function deleteComment(req, res){
+    Project.findOne({'comments._id': req.params.id}, function (err, project){
+        const commentSubdoc = project.comments.id(req.params.id);
+        commentSubdoc.remove();
+        project.save(function(err){
+            res.redirect(`/projects/${project._id}`)
+        })
+    })
+}
+
+function update(req, res) {
+    Project.findOne({'comments._id': req.params.id}, function (err, project){
+        const commentSubdoc = project.comments.id(req.params.id);
+        if (!commentSubdoc.userId.equals(req.user._id)) return res.redirect(`/projects/${project._id}`);
+        commentSubdoc.text = req.body.text;
+        project.save(function(err){
+            res.redirect(`/projects/${project._id}`)
+        })
+    })
 }
 
 function create(req, res) {
@@ -23,9 +46,10 @@ function edit(req, res){
         if(err){
             res.send(err);
         } else {
-            res.render(`projects/comments/edit`),
+            res.render(`projects/comments/edit`, {
             project,
             commentSubdoc
+            })
         }
     })
 }
--- routes/projectComments.js
+++ routes/projectComments.js
@@ -4,5 +4,7 @@ const projectCommentsCtrl = require('../controllers/projectComments');
 
 router.post('/projects/:id/comments', projectCommentsCtrl.create);
 router.get('/projects/:id/comments/:id/edit', projectCommentsCtrl.edit);
+router.put('/projects/:id/comments/:id', projectCommentsCtrl.update);
+router.delete('/projects/:id/comments/:id', projectCommentsCtrl.delete);
 
 module.exports = router;
