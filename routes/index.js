3338c812c475ea8a1d47019d91f5e4a8db947288
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2021-06-24 14:07:05 -0700

    deletes commented out code

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,24 @@
 var router = require('express').Router();
 const passport = require('passport');
 
-// The root route renders our only view
 router.get('/', function(req, res) {
   res.render('index')
 });
 
-// Google OAuth login route
 router.get('/auth/google', passport.authenticate(
   'google',
   { scope: ['profile', 'email'] }
 ));
 
-// Google OAuth callback route
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/dashboard', // where do you want the client to go after you login 
-    failureRedirect : '/' // where do you want the client to go if login fails
+    successRedirect : '/dashboard',  
+    failureRedirect : '/' 
   }
 ));
 
-// OAuth logout route
+
 router.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
